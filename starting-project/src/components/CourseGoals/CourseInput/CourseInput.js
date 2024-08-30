6a251cf1f18e99d5553bed42afd80dface233ca2
damjansmilkov <damjansmilkov3@gmail.com>
2024-08-30 15:37:43 +0200

    Update: made changes to the code so that it is better structured.

--- starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,8 +1,40 @@
 import React, { useState } from "react";
-
+import styled from "styled-components";
 import Button from "../../UI/Button/Button";
 import "./CourseInput.css";
 
+const FromControl = styled.div`
+  margin: 0.5rem 0;
+  
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    color: ${(props) => (props.invalid ? "red" : "black")};
+  }
+  
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+    background: ${(props) => (props.invalid ? "salmon" : "transparent")};
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+  &.invalid input {
+    border-color: red;
+    background-color: salmon;
+  }
+`;
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
   const [fullBox, setFullBox] = useState(true);
@@ -28,17 +60,10 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!fullBox ? 'invalid': 'valid'}`}>
-        <label >Course Goal</label>
-        <input
-          type="text"
-          onChange={goalInputChangeHandler}
-          // style={{
-          //   borderColor: !fullBox ? "red" : "black",
-          //   background: !fullBox ? "salmon" : "transparent",
-          // }}
-        />
-      </div>
+      <FromControl invalid={!fullBox}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </FromControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
